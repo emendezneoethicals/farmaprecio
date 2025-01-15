b3f8deb670d3aaee421b6469b717d6ce129759a9
Angel Mendez <emendez@neoethicals.com>
2025-01-15 17:17:27 +0000

    Modulo base

---
+++ __manifest__.py
@@ -0,0 +1,28 @@
+{
+    'name': 'FarmaPrecio',
+    'version': '1.0',
+    'summary': 'Replica del sitio web FarmaPrecio en Odoo 16',
+    'description': """
+        Este módulo implementa una página web personalizada para FarmaPrecio,
+        desarrollada en Odoo 16 Community Edition.
+    """,
+    'author': 'Angel Mendez',
+    'website': '',
+    'category': 'Website',
+    'license': 'LGPL-3',
+    'depends': ['website'], 
+    'data': [
+       
+    ],
+    'assets': {
+        'web.assets_frontend': [
+            
+        ],
+        'web.assets_qweb': [
+           
+        ],
+    },
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
